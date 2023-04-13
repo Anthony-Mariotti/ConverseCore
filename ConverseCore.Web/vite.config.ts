a1ec4986813c791f8b61cd34d54cd2caa0f80150
Anthony Mariotti <anthony.mariotti@yahoo.com>
2023-04-13 02:14:20 -0400

    Switch to svelte

--- ConverseCore.Web/vite.config.ts
+++ ConverseCore.Web/vite.config.ts
@@ -1,51 +1,44 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
-import fs from 'fs'
-import path from 'path'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+import fs from 'fs';
+import path from 'path';
 
 const baseFolder =
-  process.env.APPDATA !== undefined && process.env.APPDATA !== ''
-    ? `${process.env.APPDATA}/ASP.NET/https`
-    : `${process.env.HOME}/.aspnet/https`
+    process.env.APPDATA !== undefined && process.env.APPDATA !== ''
+        ? `${process.env.APPDATA}/ASP.NET/https`
+        : `${process.env.HOME}/.aspnet/https`;
 
 const certificateArg = process.argv
-  .map((arg) => arg.match(/--name=(?<value>.+)/i))
-  .filter(Boolean)[0]
+    .map((arg) => arg.match(/--name=(?<value>.+)/i))
+    .filter(Boolean)[0];
 
-const certificateName = certificateArg ? certificateArg.groups?.value : 'ConverseCore.Web'
+const certificateName = certificateArg ? certificateArg.groups?.value : 'ConverseCore.Web';
 
 if (!certificateName) {
-  console.error(
-    'Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.'
-  )
-  process.exit(-1)
+    console.error(
+        'Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.'
+    );
+    process.exit(-1);
 }
 
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`)
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`)
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [sveltekit()],
+    test: {
+        include: ['src/**/*.{test,spec}.{js,ts}']
+    },
+    server: {
+        https: {
+            key: fs.readFileSync(keyFilePath),
+            cert: fs.readFileSync(certFilePath)
+        },
+        proxy: {
+            '^/weatherforcast': {
+                target: 'https://localhost:7078/'
+            }
+        },
+        port: 5002
     }
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(keyFilePath),
-      cert: fs.readFileSync(certFilePath)
-    },
-    proxy: {
-      '^/weatherforcast': {
-        target: 'https://localhost:7078/'
-      }
-    },
-    port: 5002
-  }
-})
+});
