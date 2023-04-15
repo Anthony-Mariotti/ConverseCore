5b644393163fd9793d5c56f7040b63462ae324eb
Anthony Mariotti <anthony.mariotti@yahoo.com>
2023-04-15 09:12:10 -0400

    Update Breadcumb style

--- ConverseCore.Web/src/lib/components/Breadcrumb/Breadcrumb.svelte
+++ ConverseCore.Web/src/lib/components/Breadcrumb/Breadcrumb.svelte
@@ -1,9 +1,6 @@
-<nav
-    class="flex flex-col bg-gray-900 pt-2 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"
-    aria-label="Breadcrumb"
->
-    <ol class="mx-auto text-sm flex w-full max-w-screen-xl flex-wrap">
-        <li class="flex items-center">
+<nav class="flex flex-col bg-gray-800 bg-opacity-50 py-2" aria-label="Breadcrumb">
+    <ol class="mx-auto text-sm flex w-full max-w-7xl flex-wrap px-4 sm:px-6 lg:px-8">
+        <li class="hidden lg:flex items-center">
             <a href="/" class="text-gray-200 hover:text-gray-400">Home</a>
             <svg
                 class="h-5 w-5 flex-shrink-0 text-gray-200 mx-1"
@@ -19,7 +16,25 @@
             </svg>
         </li>
 
-        <li class="flex">
+        <li class="flex lg:hidden">
+            <div class="flex items-center">
+                <a href="/" class="text-gray-200 hover:text-gray-400" aria-current="page">...</a>
+                <svg
+                    class="h-5 w-5 flex-shrink-0 text-gray-200 mx-1"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                        clip-rule="evenodd"
+                    />
+                </svg>
+            </div>
+        </li>
+
+        <li class="hidden lg:flex">
             <div class="flex items-center">
                 <a href="/" class="text-gray-200 hover:text-gray-400">General</a>
                 <svg
@@ -37,7 +52,7 @@
             </div>
         </li>
 
-        <li class="flex">
+        <li class="hidden lg:flex">
             <div class="flex items-center">
                 <a href="/" class="text-gray-200 hover:text-gray-400" aria-current="page"
                     >Off Topic</a
