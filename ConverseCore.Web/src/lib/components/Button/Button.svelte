a8067e93c1514a67326b9dc2996a894665df94df
Anthony Mariotti <anthony.mariotti@yahoo.com>
2023-04-20 00:15:41 -0400

    Attempt at building a button component for tailwindcss

---
+++ ConverseCore.Web/src/lib/components/Button/Button.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let href: string | undefined = undefined;
+    export let type: 'button' | 'submit' | 'reset' = 'button';
+    export let label: string | undefined = undefined;
+    export let size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' = 'md';
+
+    const sizes = {
+        xs: '',
+        sm: '',
+        md: '',
+        lg: '',
+        xl: ''
+    };
+
+    let fullClassList: string;
+    $: fullClassList = sizes[size];
+</script>
+
+<svelte:element
+    this={href ? 'a' : 'button'}
+    type={href ? undefined : type}
+    {href}
+    {...$$restProps}
+    class={fullClassList}
+>
+    <slot>
+        <span>{label}</span>
+    </slot>
+</svelte:element>
