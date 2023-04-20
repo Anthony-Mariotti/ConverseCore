de50db3cfcfdba2c82b37dad1497d0c7e9ab0f5b
Anthony Mariotti <anthony.mariotti@yahoo.com>
2023-04-20 00:15:19 -0400

    Remove console logging

--- ConverseCore.Web/src/lib/utils/classbuilder.ts
+++ ConverseCore.Web/src/lib/utils/classbuilder.ts
@@ -1,6 +1,4 @@
 const applyClasses = (...args: string[]): string => {
-    console.log(...args);
-
     const filtered = args.filter((item) => typeof item != undefined && item);
 
     return filtered.join(' ');
